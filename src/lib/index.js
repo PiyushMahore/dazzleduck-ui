6f9c09c746aa0e256b0fff8856672a628a62a424
shrikantsuryawanshi39 <shrikantsuryawanshi155@gmail.com>
2026-03-25 21:38:40 +0530

    feat: Update package version to 1.0.15 and expand logging component exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,19 @@ export { default as LineChartD3 } from "../components/charts/LineChartD3.jsx";
 export { default as PieChartD3 } from "../components/charts/PieChartD3.jsx";
 export { formatPossibleDate } from "../components/utils/DateNormalizer.jsx";
 export { LoggingProvider, useLogging } from "../context/LoggingContext.jsx";
+
+// Logging component and its dependencies
+export { default as Logging } from "../logging/Logging.jsx";
+export { default as ConnectionPanel } from "../components/logging/ConnectionPanel.jsx";
+export { default as QueryRow } from "../components/logging/QueryRow.jsx";
+export { default as QueryResults } from "../components/logging/QueryResults.jsx";
+export { default as VariableManager } from "../components/logging/VariableManager.jsx";
+export { default as AdvancedSettings } from "../components/logging/AdvancedSettings.jsx";
+export { default as SessionManagement } from "../components/logging/SessionManagement.jsx";
+export { default as SearchTable } from "../components/logging/SearchTable.jsx";
+export { default as PopupMessage } from "../components/utils/PopupMessage.jsx";
+
+// Custom hooks for Logging
+export { useConnectionForm } from "../hooks/useConnectionForm.js";
+export { useQueryManagement } from "../hooks/useQueryManagement.js";
+export { useSessionManagement } from "../hooks/useSessionManagement.js";
